e06a40c964b07b7914746aef99f7200f9770c550
adamowada <adamowada@gmail.com>
2022-09-20 12:23:29 -0700

    demo code completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Beast from "./components/Beast.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// copy the imports!
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+// import data.json
+import data from "./assets/data.json";
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        {/* <Beast src={data[0].imgUrl} description={data[0].description}/> */}
+        {data.map((beast, index) => {
+          return <Beast src={beast.imgUrl} description={beast.description} key={index}/>
+        })}
+      </>
+    );
+  }
 }
 
 export default App;
